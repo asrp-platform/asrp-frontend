742c71738fa1d132a2fa5be5c6255bea66795c9a
Maksim <maxevg72@gmail.com>
2026-02-06 23:19:59 +0300

    feat: Created active menu item

--- src/app/(main)/(account)/account/layout.tsx
+++ src/app/(main)/(account)/account/layout.tsx
@@ -7,41 +7,52 @@ import Link from "next/link"
 
 import { useAuth } from "../../../../context/AuthProvider.tsx"
 import NotAuthorized from "../../../../shared/ui/NotAuthorized/NotAuthorized.tsx"
+import clsx from "clsx"
+import { usePathname } from "next/navigation"
 
 interface NavListItem {
     href: string
     label: string
+    match: string[]
 }
 
 const navItemsList: NavListItem[] = [
     {
         href: "/account/dashboard",
         label: "Dashboard",
+        match: ["/account/dashboard"],
     },
     {
         href: "/account/profile",
         label: "Profile",
+        match: ["/account/profile"],
     },
     {
         href: "/account/membership",
         label: "Membership",
+        match: ["/account/membership"],
     },
     {
         href: "/account/payments",
         label: "Payments",
+        match: ["/account/payments"],
     },
     {
         href: "/account/communication-preferences",
         label: "Communication Preferences",
+        match: ["/account/communication-preferences"],
     },
     {
         href: "/account/security",
         label: "Security",
+        match: ["/account/security"],
     },
 ]
 
 const Layout = ({ children }: { children: ReactNode }) => {
     const { user } = useAuth()
+
+    const pathname = usePathname()
 
     if (!user) {
         return <NotAuthorized />
@@ -54,8 +65,14 @@ const Layout = ({ children }: { children: ReactNode }) => {
                 <nav className={styles.accountNav}>
                     <ul className={styles.accountNavList}>
                         {navItemsList.map((item: NavListItem) => (
-                            <li className={styles.accountNavItem}>
-                                <Link className={styles.navItemLink} href={item.href}>
+                            <li className={styles.accountNavItem} key={item.label}>
+                                <Link
+                                    className={clsx(
+                                        styles.navItemLink,
+                                        item.match.includes(pathname) && styles.activeLink
+                                    )}
+                                    href={item.href}
+                                >
                                     {item.label}
                                 </Link>
                             </li>
