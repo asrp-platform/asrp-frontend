0d76e1f478a29b019236f3b7d7c6e335dc68cf39
Maksim <maxevg72@gmail.com>
2026-02-18 18:53:50 +0300

    feat: Accordion for about us in mobile menu

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,27 +1,44 @@
-
-
-
-
 import styles from "./Header.module.scss"
-import Logo from "../../shared/ui/LogoPlaceholder/Logo.tsx";
-import AuthStatus from "./ui/AuthStatus.tsx";
+import Logo from "../../shared/ui/LogoPlaceholder/Logo.tsx"
+import AuthStatus from "./ui/AuthStatus.tsx"
 
-import {headerMenuItems} from "./headerMenuItems.tsx";
-import SideMenu from "./ui/SideMenu.tsx";
-
+import { type HeaderMenuItem, headerMenuItems } from "./headerMenuItems.tsx"
+import SideMenu from "./ui/SideMenu.tsx"
+import { Dropdown } from "antd"
 
 const Header = () => {
-
     return (
         <header className={styles.headerElement}>
             <div className={styles.horizontalMenu}>
                 <Logo />
                 <nav className={styles.navbar}>
-                    {headerMenuItems.map((item) =>
-                        <div key={item.key}>
-                            {item.element? item.element: <a className={styles.headerMenuItem} href={item.to} >{item.label}</a>}
-                        </div>
-                    )}
+                    {headerMenuItems.map((item) => {
+                        if (item.children) {
+                            return (
+                                <Dropdown
+                                    key={item.key}
+                                    menu={{
+                                        items: item.children.map((item: HeaderMenuItem) => ({
+                                            key: item.key,
+                                            label: item.label,
+                                        })),
+                                    }}
+                                >
+                                    <a className={styles.headerMenuItem} href={item.to}>
+                                        {item.label}
+                                    </a>
+                                </Dropdown>
+                            )
+                        }
+
+                        return (
+                            <div key={item.key}>
+                                <a className={styles.headerMenuItem} href={item.to}>
+                                    {item.label}
+                                </a>
+                            </div>
+                        )
+                    })}
                 </nav>
 
                 <div className={styles.authStatusContainer}>
@@ -32,7 +49,6 @@ const Header = () => {
                     <SideMenu />
                 </div>
             </div>
-
         </header>
     )
 }
