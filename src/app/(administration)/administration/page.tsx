9ecf7117bfcf8867471b6b4ba64a755bb39ec2f6
Maksim <maxevg72@gmail.com>
2026-02-23 00:50:40 +0300

    feat: Created admin layout

---
+++ src/app/(administration)/administration/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+const Page = () => {
+    return <div>Hello</div>
+}
+
+export default Page
---
+++ src/app/(administration)/layout.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import { Layout, Menu } from "antd"
+import { UserOutlined, TeamOutlined, MailOutlined, DashboardOutlined } from "@ant-design/icons"
+import { usePathname, useRouter } from "next/navigation"
+import { type ReactNode, useMemo, useState } from "react"
+import styles from "./layout.module.scss"
+
+const { Header, Sider, Content } = Layout
+
+interface IProps {
+    children: ReactNode
+}
+
+const menuItems = [
+    {
+        key: "/administration",
+        icon: <DashboardOutlined />,
+        label: "Dashboard",
+    },
+    {
+        key: "/administration/users",
+        icon: <UserOutlined />,
+        label: "Users",
+    },
+    {
+        key: "/administration/applications",
+        icon: <TeamOutlined />,
+        label: "Applications",
+    },
+    {
+        key: "/administration/messages",
+        icon: <MailOutlined />,
+        label: "Messages",
+    },
+]
+
+const AdminLayout = ({ children }: IProps) => {
+    const router = useRouter()
+    const pathname = usePathname()
+    const [collapsed, setCollapsed] = useState(false)
+
+    const selectedKey = useMemo(() => {
+        const match = menuItems.find((item) => pathname === item.key)
+        return match ? match.key : "/administration"
+    }, [pathname])
+
+    return (
+        <Layout className={styles.layout}>
+            <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
+                <div className={styles.siderTitle}>
+                    {collapsed ? "ASRP" : "ASRP Administration"}
+                </div>
+
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    selectedKeys={[selectedKey]}
+                    items={menuItems}
+                    onClick={({ key }) => router.push(key)}
+                />
+            </Sider>
+
+            <Layout>
+                <Header className={styles.layoutHeader}>Administrative Panel</Header>
+
+                <Content className={styles.content}>{children}</Content>
+            </Layout>
+        </Layout>
+    )
+}
+
+export default AdminLayout
