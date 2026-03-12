0e1fed5e0cc8d424f82ca596d939fb49efd27d0b
Maksim <maxevg72@gmail.com>
2026-03-12 20:32:14 +0300

    feat: Added primary button

---
+++ src/shared/ui/Buttons/PrimaryButton.tsx
@@ -0,0 +1,21 @@
+import clsx from "clsx"
+
+import styles from "./Buttons.module.scss"
+import type { MouseEventHandler } from "react"
+
+interface IProps {
+    label: string
+    className?: string
+    onClick?: MouseEventHandler<HTMLButtonElement> | undefined
+    htmlType?: "button" | "submit" | "reset"
+}
+
+const PrimaryButton = ({ label, className, onClick, htmlType = "button" }: IProps) => {
+    return (
+        <button className={clsx(styles.primaryButton, className)} onClick={onClick} type={htmlType}>
+            {label}
+        </button>
+    )
+}
+
+export default PrimaryButton
