f7e3d10aa49e95cdab8ebd3741a7bb8c191f9926
Maksim <maxevg72@gmail.com>
2026-04-10 15:12:17 +0300

    feat: Added payment to user profile

--- src/shared/backend/currentUserUrls.ts
+++ src/shared/backend/currentUserUrls.ts
@@ -9,3 +9,5 @@ export const CURRENT_USER_CHANGE_PASSWORD_URL = `${CURRENT_USER_URL}/password-ch
 export const CURRENT_USER_NAME_CHANGE_URL = `${CURRENT_USER_URL}/name-change-requests`
 
 export const CURRENT_USER_MEMBERSHIP_URL = `${CURRENT_USER_URL}/membership-requests`
+
+export const CURRENT_USER_PAYMENTS_URL = `${CURRENT_USER_URL}/payments`
