106622c78adca6a8bf34693b41a46c07033bbf1f
Maksim <maxevg72@gmail.com>
2026-06-24 22:24:01 +0300

    fix: Fixed CI

--- src/app/(administration)/administration/membership-requests/ui/ActionsCell/ActionsCell.tsx
+++ src/app/(administration)/administration/membership-requests/ui/ActionsCell/ActionsCell.tsx
@@ -4,8 +4,8 @@ import { Alert, Button, Input, Modal } from "antd"
 import styles from "./ActionsCell.module.scss"
 import { MembershipRequestStatusEnum } from "@entities/Membership.ts"
 import api from "@/axios.ts"
-import { MEMBERSHIP_REQUESTS_ADMIN_URL } from "@shared/backend/rest-api-urls/admin/membershipsUrls.ts"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { MEMBERSHIP_REQUESTS_ADMIN_URL } from "@/shared/backend/rest-api-urls/admin/membershipsAdminUrls"
 
 type UpdateMembershipRequestPayload = {
     requestId: number | string
