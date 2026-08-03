edc0cbd108760fec3e363cd0b14fca2ce5b1d6c0
Maksim <maxevg72@gmail.com>
2026-08-03 21:25:45 +0300

    feat: Fixed mobile styles

--- src/app/(main)/education/webinars/(ui)/UpcomingWebinarsSection/components/CreateWebinarModal/CreateWebinarModal.tsx
+++ src/app/(main)/education/webinars/(ui)/UpcomingWebinarsSection/components/CreateWebinarModal/CreateWebinarModal.tsx
@@ -1,14 +1,17 @@
 "use client"
 
 import CustomButton from "@shared/ui/Buttons/CustomButton.tsx"
+import { Plus } from "lucide-react"
 
 import WebinarFormModal from "../WebinarFormModal/WebinarFormModal"
+import styles from "./CreateWebinarModal.module.scss"
 
 const CreateWebinarModal = () => (
     <WebinarFormModal
         renderTrigger={(openModal) => (
-            <CustomButton onClick={openModal} variant="primary">
-                Create a new webinar
+            <CustomButton onClick={openModal} variant="primary" className={styles.trigger}>
+                <Plus size={17} />
+                <span>Create a new webinar</span>
             </CustomButton>
         )}
     />
